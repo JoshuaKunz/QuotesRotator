8bbf609f5e5bcbbfb66d75bf7dfa03bd98ed378f
Josh Kunz <engndeveloper@gmail.com>
2020-01-28 10:15:00 -0600

    Initial commit.

---
+++ QuotesRotator_jkunz2/js/jquery.qtRotator.js
@@ -0,0 +1,30 @@
+
+       /* 
+        if (typeof options === 'string') {
+            
+            // not as common, leave off code for now...
+            
+        }
+        else {  // options !== 'string', usually meaning it is an object
+            
+            // here, this refers the jQuery object that was used
+            // to call this plugin method ($('#quoteRotator'))
+            this.each(function() {
+                
+                var instance = $.data(this, 'qtRotator');
+                
+                if (instance) {
+                    instance._init();
+                }
+                else {
+                    
+                    
+                    
+                }
+                
+            });
+            
+        }
+		*/
+        
+        
