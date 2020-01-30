b8ee42bcaa8829af75e83d3ff166b9e2e15cd84d
Josh Kunz <engndeveloper@gmail.com>
2020-01-30 11:54:37 -0600

    After class thursday

--- QuotesRotator_jkunz2/js/jquery.qtRotator.js
+++ QuotesRotator_jkunz2/js/jquery.qtRotator.js
@@ -1,30 +1,41 @@
+; (function ($, window, undefined) {
+    //Ctor
 
-       /* 
+    //Default Property
+
+    //Define property of our plugin object
+
+    //Define methods of our plugin object
+
+    //Define our new jquery method definition (qtRotator)
+    $.fn.qtRotator = function (options) {
         if (typeof options === 'string') {
-            
+
             // not as common, leave off code for now...
-            
+
         }
         else {  // options !== 'string', usually meaning it is an object
-            
+
             // here, this refers the jQuery object that was used
             // to call this plugin method ($('#quoteRotator'))
-            this.each(function() {
-                
+            this.each(function () {
+
                 var instance = $.data(this, 'qtRotator');
-                
+
                 if (instance) {
                     instance._init();
                 }
                 else {
-                    
-                    
-                    
+
+
+
                 }
-                
+
             });
-            
+
         }
-		*/
-        
-        
+    };
+
+
+
+})(jQuery, window);
