be4c7c92c82c31eac673068af414be26bb6e585e
Josh Kunz <engndeveloper@gmail.com>
2020-02-04 10:52:23 -0600

    plugin template

--- QuotesRotator_jkunz2/js/jquery.qtRotator.js
+++ QuotesRotator_jkunz2/js/jquery.qtRotator.js
@@ -1,14 +1,36 @@
 ; (function ($, window, undefined) {
-    //Ctor
 
-    //Default Property
+    'use strict'; //requires the var keyword for variables and other more strict syntax.
+    var Modernizr = window.Modernizr;
+
+    //Ctor
+    $.QTRotator = function (options, divQuoteRotator) {
+        //save the passed in DOM element into a property of our 
+        //plugin object 
+        //and make sure this property is a JQuery object
+        this.$element = $(divQuoteRotator);
+
+        //get the party started by calling an initial method
+        //of this plugin class
+        this._init(options);
+    };
+
+    //Defaults Property
+    $.QTRotator.defaults = {
+        speed: 700,
+        easing: 'ease',
+        interval: 8000
+    };
+
 
     //Define property of our plugin object
+
 
     //Define methods of our plugin object
 
     //Define our new jquery method definition (qtRotator)
     $.fn.qtRotator = function (options) {
+
         if (typeof options === 'string') {
 
             // not as common, leave off code for now...
@@ -20,22 +42,31 @@
             // to call this plugin method ($('#quoteRotator'))
             this.each(function () {
 
+                //Here, this refers to div.quoteRotator because
+                //n a JQuery each() method's function parameter,
+                //the this keyword refers to the current
+                //matched DOM element that is currewntly
+                //iterating (looping) over
                 var instance = $.data(this, 'qtRotator');
 
                 if (instance) {
                     instance._init();
                 }
                 else {
+                    //call from low-level (utility) JQuery data() method again
+                    //this time in setter mode (3 paramaters) to create a named 
+                    //data store named 'qtrotator' in the DOM on the div.quoteRotator tag.
 
-
+                    instance = $.data(this, 'qtRotator', new $.QTRotator(options, this));
 
                 }
 
             });
 
         }
+
+        return this; // make this method chainable because it returns it's JQuery object.
+
     };
-
-
 
 })(jQuery, window);
